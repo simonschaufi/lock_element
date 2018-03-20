9e4e248fb077e5f7bf3d6df09285576e58963704
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2018-03-20 18:00:02 +0100

    Move code into sub folders

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,18 +1,14 @@
 <?php
-defined('TYPO3_MODE') or die();
 
 $tempColumns = [
     'tx_lockelement_locked' => [
         'exclude' => 1,
-        'label' => 'LLL:EXT:lock_element/locallang_db.xml:tt_content.tx_lockelement_locked',
+        'label' => 'LLL:EXT:lock_element/Resources/Private/Language/locallang_db.xml:tt_content.tx_lockelement_locked',
         'config' => [
             'type' => 'check',
         ]
     ],
 ];
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumns);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'tx_lockelement_locked;;;;1-1-1');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $tempColumns);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages', 'tx_lockelement_locked;;;;1-1-1');
---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,14 @@
+<?php
+
+$tempColumns = [
+    'tx_lockelement_locked' => [
+        'exclude' => 1,
+        'label' => 'LLL:EXT:lock_element/Resources/Private/Language/locallang_db.xml:tt_content.tx_lockelement_locked',
+        'config' => [
+            'type' => 'check',
+        ]
+    ],
+];
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumns);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'tx_lockelement_locked;;;;1-1-1');
