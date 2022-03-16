5e7297cc00acc3f477b489ffdbf2660614523419
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2022-03-16 02:40:49 +0100

    [TASK] Add allowed composer plugins

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.4.0-11.99.99',
+            'typo3' => '11.5.0-11.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
